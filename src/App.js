c4eb1e44d0b7de40ff5618e8d6164f76c2ec674b
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-01 17:29:12 +0530

    adding str

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Landing from "./components/landing/Landing"
+import './App.css'
+import WebImage from "./components/web/WebImage";
+import Feature from "./components/features/Feature";
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+      <WebImage />
+      <Feature />
+    </div>
+  );
+}
+
+export default App;
