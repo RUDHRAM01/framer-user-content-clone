4221d68c99f22e6b598f45d54ee1184939abbdd4
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-01 17:29:55 +0530

    adding webimage

---
+++ src/components/web/WebImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WebImage() {
+    return (
+        <div className='WebImage'>   
+            <img src="https://framerusercontent.com/images/t9qCSNfZSNdBCMSiohCGNEIl4.jpg?scale-down-to=2048" alt="img" className='Image'/>
+        </div>
+    )
+}
+
+export default WebImage
