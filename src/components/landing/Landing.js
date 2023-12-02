b7783977a36b649075bb53d3a230effdd651ec7b
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-02 22:40:53 +0530

    update

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Nav from '../nav/Nav';
 import TagLine from './TagLine';
 
-function Landing() {
+function Landing({ isMobile }) {
+    
     const data = {
         icon: "👋",
         title1: "WELCOME TO MANAGE WISE!",
-        size : {title1 : "74px"},
+        size : isMobile ? {title1 : "40px"} : {title1 : "74px"},
         title2: "Empower your business with",
         highlighted: "Strategic",
         title3: "insights",
@@ -14,8 +15,8 @@ function Landing() {
     }
     return (
         <div className='landing'>
-            <Nav />
-            <TagLine data={data} button={true} />
+            <Nav isMobile={isMobile} />
+            <TagLine data={data} button={true} buttonS={true} value="Get Started" />
         </div>
     )
 }
