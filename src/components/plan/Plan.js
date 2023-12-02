39025ca82350d4c99d7402b8faf401dd85d63abe
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-02 12:06:17 +0530

    adding plan

---
+++ src/components/plan/Plan.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import TagLine from '../landing/TagLine'
+import Card from './Card'
+
+function Plan() {
+    const data = {
+        icon: "💲",
+        title1: "PRICING",
+        size: { title1: "50px" },
+        title2: "Select your ideal",
+        highlighted: "Pricing",
+        title3: "plan",
+        title4: "At Manage Wise, we believe in providing you with pricing plans that adapt to your unique needs.",
+    }
+    const plans = [
+        {
+            type: "FREE",
+            price: "$0",
+            features: [
+                "Access to all basic features", "Reporting and analytics", "Up to 5 individual users", "Chat and email support"
+            ]
+        },
+        {
+            type: "STANDARD",
+            price: "$25",
+            features: [
+                "Access to all basic features", "Reporting and analytics", "Up to 5 individual users", "Chat and email support", "3+ integrations", "Account performance reporting"
+            ]
+        },
+        {
+            type: "BUSINESS",
+            price: "$42",
+            features: [
+                "Access to all basic features", "Reporting and analytics", "Up to 5 individual users", "Chat and email support", "Account performance reporting"
+            ]
+        }
+    ]
+    return (
+        <div className='plan'>
+            <TagLine data={data} />
+            <div className='planCard'>
+                {plans.map((value, i) =>
+                    <Card data={value} i={i} />
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Plan
