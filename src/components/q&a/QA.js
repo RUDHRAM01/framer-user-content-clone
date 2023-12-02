d9eb199e7fb07c9b733f0b166cd8d84e6033282d
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-02 22:59:36 +0530

    update

--- src/components/q&a/QA.js
+++ src/components/q&a/QA.js
@@ -2,11 +2,11 @@ import React from 'react'
 import TagLine from '../landing/TagLine'
 import Accordions from './Accordion'
 
-function QA() {
+function QA({isMobile}) {
   const data = {
     icon: "🙋‍♀️",
     title1: "FAQ",
-    size: { title1: "50px" },
+    size: isMobile ? { title1: "30px" } : { title1: "50px" },
     title2: "Need",
     highlighted: "Answers?",
     title4: "Check out our most commonly asked questions below to find the information you need.",
