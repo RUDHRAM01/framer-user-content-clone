5336ae06beb9a7b82188995202ff08ef8ebe6c5a
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-02 12:05:33 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import Landing from "./components/landing/Landing"
 import './App.css'
 import WebImage from "./components/web/WebImage";
 import Feature from "./components/features/Feature";
+import MoreFeature from "./components/moreFeature/MoreFeature";
+import QA from "./components/q&a/QA";
+import Plan from "./components/plan/Plan";
+import Testimonials from "./components/testimonials/Testimonials";
+
 function App() {
   return (
     <div className="App">
       <Landing />
       <WebImage />
       <Feature />
+      <MoreFeature />
+      <QA />
+      <Plan />
+      <Testimonials />
     </div>
   );
 }
