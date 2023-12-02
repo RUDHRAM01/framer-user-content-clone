9a9ae6a26531d822b4de102cd83c13928717b892
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-02 22:41:03 +0530

    update

--- src/components/moreFeature/Card.js
+++ src/components/moreFeature/Card.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Card(props) {
     return (
         <div className='moreCard'>
-            <div style={{fontSize:"40px",backgroundColor:"#8247ff",display:"flex",alignItems:"center",justifyContent:"center",padding:"10px",borderRadius:"50%"}}>
+            <div style={{backgroundColor:"#8247ff",display:"flex",alignItems:"center",justifyContent:"center",padding:"10px",borderRadius:"50%"}} className='moreCI'>
                 {props?.data?.logo}
             </div>
             <p style={{ color: "white", fontSize: "24px", fontWeight: "600", marginBottom: "0%" }}>{props?.data?.title}</p>
