fe7eb2247d4cc341dfaefb864a468851e4bcd9e5
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-02 22:40:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,17 +6,34 @@ import MoreFeature from "./components/moreFeature/MoreFeature";
 import QA from "./components/q&a/QA";
 import Plan from "./components/plan/Plan";
 import Testimonials from "./components/testimonials/Testimonials";
+import Join from "./components/joinCard/Join";
+import Bottom from "./components/bottom/Bottom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkMobileWidth = () => {
+      const mobileWidthThreshold = 800;
+      setIsMobile(window.innerWidth < mobileWidthThreshold);
+    };
+    window.addEventListener('resize', checkMobileWidth);
+    checkMobileWidth();
+    return () => {
+      window.removeEventListener('resize', checkMobileWidth);
+    };
+  }, []);
   return (
     <div className="App">
-      <Landing />
+      <Landing isMobile={isMobile}/>
       <WebImage />
-      <Feature />
-      <MoreFeature />
+      <Feature isMobile={isMobile}/>
+      <MoreFeature isMobile={isMobile}/>
       <QA />
       <Plan />
       <Testimonials />
+      <Join />
+      <Bottom />
     </div>
   );
 }
